35ad6516fd00c428ceb8dcb9e87e2d04355c6c88
leehyeryeong <w2108@e-mirim.hs.kr>
2023-01-18 23:14:54 +0900

    멀티 스레드 postMessage로 데이터 보내기

--- worker_threads.js
+++ worker_threads.js
@@ -1,29 +1,14 @@
-const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
+const { Worker, isMainThread, parentPort } = require('worker_threads');
 
 if(isMainThread) {  //메인 스레드
-    const threads = new Set();
-    threads.add(new Worker(__filename, {
-        workerData: {start: 1},
-    }));
-    threads.add(new Worker(__filename, {
-        workerData: {start: 2},
-    }));
     const worker = new Worker(__filename);
-    worker.on('message', (value) => console.log('워커로부터', value));
-    worker.on('exit', () => {
-        threads.delete(worker);
-        if(threads.size === 0) {
-            console.log('워커 끝~');
-        }
-    });
-
+    worker.on('message', message => console.log('from worker', message));
+    worker.on('exit', () => console.log('worker exit'));
     worker.postMessage('ping');
 } else {            //워커 스레드
-    // parentPort.on('message', (value) => {
-    //     console.log('부모로부터', value);
-    //     parentPort.postMessage('pong');
-    //     parentPort.close();
-    // })
-    const data = workerData;
-    parentPort.postMessage(data.start + 100);
+    parentPort.on('message', (value) => {
+        console.log('from parent', value);
+        parentPort.postMessage('pong');
+        parentPort.close();
+    });
 }
