1a29e45d017e07162a08e7bde41d8c70e73ba0f4
leehyeryeong <w2108@e-mirim.hs.kr>
2023-02-20 23:22:24 +0900

    child process

--- exec.js
+++ exec.js
@@ -4,8 +4,8 @@ var process = exec('dir');
 
 process.stdout.on('data', function(data) {
     console.log(data.toString('utf8'));
-})
+});
 
 process.stderr.on('data', function(data) {
     console.error(data.toString('utf8'));
-})
+});
