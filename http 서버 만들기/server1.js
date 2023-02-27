73f4615802f11e89de04250602c1a34aa5b41670
leehyeryeong <w2108@e-mirim.hs.kr>
2023-02-27 21:19:10 +0900

    비동기에서도 에러가 날 수 있으므로 에러 처리 하기

--- http 서버 만들기/server1.js
+++ http 서버 만들기/server1.js
@@ -1,10 +1,14 @@
 const http = require('http');
 
-http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
     res.write('<h1>Hello Node</h1>');
     res.write('<p>Hello server</p>');
     res.end('<p>Hello ZeroCho</p>');
 })
-    .listen(8080, () => {
-        console.log('8080번 포트에서 서버 대기 중입니다.');
-    });
+    .listen(8080);
+server.on('listening', () => {
+    console.log('8080번 포트에서 서버 대기 중입니다.');
+});
+server.on('error', () => {
+    console.error(error);
+});
